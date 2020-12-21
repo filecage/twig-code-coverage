44788268d8edb1da12cbf0444c6535e900043840
filecage <mail@dbeuchert.com>
2020-12-21 17:07:10 +0100

    updates wrap syntax, get templateName from token

--- src/TwigAdapter/TracerWrapParser.php
+++ src/TwigAdapter/TracerWrapParser.php
@@ -2,12 +2,24 @@
 
     namespace Thomann\TwigCodeCoverage\TwigAdapter;
 
+    use Thomann\TwigCodeCoverage\InjectionLoader;
     use Thomann\TwigCodeCoverage\Tracer;
     use Twig\Token;
     use Twig\TokenParser\AbstractTokenParser;
 
     class TracerWrapParser extends AbstractTokenParser {
-        private const TWIG_TAG = '__templateCoverage';
+        /**
+         * @internal
+         * @see InjectionLoader will be added automatically
+         */
+        const TWIG_WRAP_TAG_START = '__templateCoverage';
+
+        /**
+         * @internal
+         * @see InjectionLoader will be added automatically
+         */
+        const TWIG_WRAP_TAG_END = '__templateCoverage_end';
+
         private Tracer $tracer;
 
         function __construct (Tracer $tracer) {
@@ -16,18 +28,18 @@
 
         function parse(Token $token) {
             $stream = $this->parser->getStream();
+            $templateName = $stream->expect(Token::STRING_TYPE);
 
             $stream->expect(/* Token::BLOCK_END_TYPE */ 3);
-            $children = $this->parser->subparse(fn(Token $token) => $token->test('end__templateCoverage'));
+            $children = $this->parser->subparse(fn(Token $token) => $token->test(self::TWIG_WRAP_TAG_END));
 
-            $stream->expect(Token::NAME_TYPE, 'end' . self::TWIG_TAG);
+            $stream->expect(Token::NAME_TYPE, self::TWIG_WRAP_TAG_END);
             $stream->expect(Token::BLOCK_END_TYPE);
 
-            // todo: replace internal `getSourceComponent` with something from the public API
-            return new TracerWrapNode($this->tracer, $children, $token->getLine(), $stream->getSourceContext()->getName());
+            return new TracerWrapNode($this->tracer, $children, $token->getLine(), $templateName->getValue());
         }
 
         function getTag() {
-            return self::TWIG_TAG;
+            return self::TWIG_WRAP_TAG_START;
         }
     }
